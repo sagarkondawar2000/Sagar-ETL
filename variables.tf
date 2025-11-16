85a15133e21c9b8baf01be73ba2aca1b99739bfa
Sagar Kondawar <sagarkondawar2000@gmail.com>
2025-11-16 23:29:48 +0530

    changed variable

--- variables.tf
+++ variables.tf
@@ -1,2 +1,13 @@
-variable "bucket_name" { default = "sagar-employees-bucket" }
-variable "region" { default = "us-east-1" }
+variable "bucket_name" {
+  default = "sagar-employees-bucket"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "env" {
+  description = "Environment name (dev or prod)"
+  type        = string
+  default     = "dev"
+}
